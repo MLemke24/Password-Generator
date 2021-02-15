05e09ad242df8696250c7011f044b2c824b0eab9
Matt Lemke <mattrlemke@gmail.com>
2021-02-15 10:27:42 -0600

    figured our random array

--- Develop/script.js
+++ Develop/script.js
@@ -1,35 +1,99 @@
-// Assignment code here
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-var length;
+// Assignment code here
 
-var lowercase 
 
-var uppercase 
+var length, uppercase, lowercase, special, numbers
 
-var numbers 
+var uppercaseLettersArray = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+];
+var lowercaseLettersArray = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
+var numbersArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specialCharactersArray = [
+  '!',
+  '#',
+  '$',
+  '%',
+  '&',
+  '(',
+  ')',
+  '*',
+  '+',
+  '-',
+  '.',
+  '/',
+  ':',
+  ';',
+  '<',
+  '=',
+  '>',
+  '?',
+  '@',
+];
 
-var special
 
-var options
 
-function passwordLength () {
-
-  length = window.prompt("How many characters would you like your password to be? Please choose between 8 and 128.");
+  length = parseInt(window.prompt("How many characters would you like your password to be? Please choose between 8 and 128."));
   console.log(length);
   //  return(length);
-  if (length > 7 && length < 129) {
-    alert("Great! Lets Get Started.")
+  if (length < 7 && length > 128) {
+    alert("Not a valid response")
     } else {
-      if (isNaN(length) || length === "" || length === null) {
-        window.alert("Please enter a valid number.");
-    }
-    parseInt(length);
-  } 
-};
 
-function passwordOptions() {
   lowercase = confirm("Do you want to include lowercase letters? Please select OK for 'Yes' and CANCEL for 'No'");
 
   uppercase = confirm("Do you want to include uppercase letters? Please select OK for 'Yes' and CANCEL for 'No'");
@@ -38,134 +102,43 @@ function passwordOptions() {
 
   special = confirm("Do you want to include special characters? Please select OK for 'Yes' and CANCEL for 'No'");
 
-  return options = {
-    lowercase: lowercase,
-    uppercase: uppercase,
-    numbers: numbers,
-    special: special,
+  generatePassword(length, uppercase, lowercase, numbers, special);
   };
-
-}
-
-
-
+ 
 function generatePassword () {
-  length = passwordLength();
-  options = passwordOptions();
-  console.log(options);
-  var uppercaseLettersArray = [
-    'A',
-    'B',
-    'C',
-    'D',
-    'E',
-    'F',
-    'G',
-    'H',
-    'I',
-    'J',
-    'K',
-    'L',
-    'M',
-    'N',
-    'O',
-    'P',
-    'Q',
-    'R',
-    'S',
-    'T',
-    'U',
-    'V',
-    'W',
-    'X',
-    'Y',
-    'Z',
-  ];
-  var lowercaseLettersArray = [
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f',
-    'g',
-    'h',
-    'i',
-    'j',
-    'k',
-    'l',
-    'm',
-    'n',
-    'o',
-    'p',
-    'q',
-    'r',
-    's',
-    't',
-    'u',
-    'v',
-    'w',
-    'x',
-    'y',
-    'z',
-  ];
-  var numbersArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  var specialCharactersArray = [
-    '!',
-    '#',
-    '$',
-    '%',
-    '&',
-    '(',
-    ')',
-    '*',
-    '+',
-    '-',
-    '.',
-    '/',
-    ':',
-    ';',
-    '<',
-    '=',
-    '>',
-    '?',
-    '@',
-  ];
-
-
   var possiblePasswordArray = []
   var passwordArray = []
 
-  if(options.lowercase) {
+  if(lowercase) {
     possiblePasswordArray =  possiblePasswordArray.concat(lowercaseLettersArray);
   }
 
-  if(options.uppercase) {
+  if(uppercase) {
     possiblePasswordArray =  possiblePasswordArray.concat(uppercaseLettersArray);
   }
 
-  if(options.numbers) {
+  if(numbers) {
     possiblePasswordArray =  possiblePasswordArray.concat(numbersArray);
   }
 
-  if(options.special) {
+  if(special) {
     possiblePasswordArray =  possiblePasswordArray.concat(specialCharactersArray);
   }
 
 console.log(possiblePasswordArray);
 
-
-
 console.log(passwordArray);
-
-debugger;
-
-};
 
 for (var i = 0; i < length; i++) {
   passwordArray = passwordArray.concat(possiblePasswordArray[Math.floor(Math.random() *  possiblePasswordArray.length)]);
 
-}
+
+};
+
+
+
+
+};
 
 // Write password to the #password input
 function writePassword() {
@@ -175,8 +148,6 @@ function writePassword() {
   passwordText.value = password;
 
 };
-
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
